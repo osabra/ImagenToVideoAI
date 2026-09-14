87556128485c11b370b9d577959cd5429492554b
osabra <arezvo92@gmail.com>
2026-09-14 17:44:15 +0200

    Connect Android app to image-to-video backend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,11 @@ plugins {
 }
 
 android {
-    namespace = "com.osabra.imag en tov ideoai".replace(" ", "")
+    namespace = "com.osabra.imagentovideoai"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.osabra.imag en tov ideoai".replace(" ", "")
+        applicationId = "com.osabra.imagentovideoai"
         minSdk = 26
         targetSdk = 36
         versionCode = 1
@@ -40,4 +40,5 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
     implementation("androidx.activity:activity-ktx:1.10.1")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
 }
